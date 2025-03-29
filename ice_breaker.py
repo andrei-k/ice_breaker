2f7166e9d3bdb5241779dbed308d3e71c5077688
Andrei K. <andrei@12ak.com>
2025-03-28 20:48:30 -0400

    Add comment

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,5 +1,6 @@
 import os
 
+# Checks if the script is being run directly (as opposed to being imported as a module in another script). If it is being run directly, the code block under this condition will execute.
 if __name__ == "__main__":
     print("Hello Langchain!")
     print(os.environ["OPENAI_API_KEY"])
