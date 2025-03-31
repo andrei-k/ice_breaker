6b3c9708b36729e0e7df6595f19fcb3be2c11a5e
Andrei K. <andrei@12ak.com>
2025-03-30 21:15:51 -0400

    Use python-dotenv to load env variables

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,6 +1,13 @@
+from dotenv import load_dotenv
 import os
 
 # Checks if the script is being run directly (as opposed to being imported as a module in another script). If it is being run directly, the code block under this condition will execute.
 if __name__ == "__main__":
-    print("Hello Langchain!")
-    print(os.environ["OPENAI_API_KEY"])
+    load_dotenv()
+    print("Hello Langchain!!")
+
+    try:
+        print(os.environ["COOL_API_KEY"])
+        print(os.environ["OPENAI_API_KEY"])
+    except KeyError as e:
+        print(f"Error: Missing environment variable {e}")
